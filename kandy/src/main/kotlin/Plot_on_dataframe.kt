f5344b842c227ec56a26f4ae5d6ad978b08913e4
Aleksandr Nikolaev <aleksandr.nikolaev@jetbrains.com>
2026-04-29 22:52:03 +0200

    Replaced `writeCsv` with `writeJson` and removed unused imports

--- kandy/src/main/kotlin/Plot_on_dataframe.kt
+++ kandy/src/main/kotlin/Plot_on_dataframe.kt
@@ -1,11 +1,7 @@
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.kandy.dsl.plot
 import org.jetbrains.kotlinx.kandy.letsplot.export.save
-import org.jetbrains.kotlinx.kandy.letsplot.feature.layout
 import org.jetbrains.kotlinx.kandy.letsplot.layers.area
-import org.jetbrains.kotlinx.kandy.letsplot.layers.path
-import org.jetbrains.kotlinx.kandy.letsplot.settings.LineType
-import org.jetbrains.kotlinx.kandy.util.color.Color
 
 
 fun buildPlotOnDataFrame() {
